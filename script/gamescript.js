ece44caa5cbea1f1b3f97c6b6cd3218b5c6509b2
Sang Min Na <xsangmin@gmail.com>
2016-10-04 14:36:29 -0400

    place player in the middle

--- script/gamescript.js
+++ script/gamescript.js
@@ -1,7 +1,10 @@
 // dom content loaded
 $(document).ready(function() {
   var player = $('.player');
-  var sky = $('.rock-container');
+  var windowWidth = $('body').width();
+  var playerWidth = player.width();
+  var playerPosition = (windowWidth/2) - (playerWidth/2);
+  player.css('left', playerPosition);
 
   function move(event) {
     var left = player.offset().left;
@@ -30,35 +33,51 @@ $(document).ready(function() {
   // draw divs as burning rocks
   function draw() {
     var edge = $('body').width();
+    var sky = $('.rock-container');
     var $newRock = $('<div>');
     $newRock.addClass('rocks');
 
     // randomize rock's width and height
-    var width =   Math.round((Math.random() * 130) + 50);
-    var height =  Math.round((Math.random() * 80) + 50);
+    var width =   Math.round((Math.random() * 100) + 50);
+    var height =  Math.round((Math.random() * 60) + 30);
     console.log(width, height);
     var top = 0;
     var left = Math.round(Math.random() * (edge - 60));
     console.log(top, left);
 
+    /**
+      * Need to make sure the rocks don't overlap
+      *
+    **/
+
     $newRock.css({'width': width, 'height': height, 'background-color': 'red', 'display': 'inline-block', 'position': 'absolute', 'top': top, 'left': left});
     sky.append($newRock);
+
+    // make it rain flaming rocks
+    fall();
   }
 
   // update periodically to speed up
-  function update() { }
+  function update() {}
 
   // update the rock's top position incrementally
   // incremental value gets higher when the pace increases
-  function fall() {
-    var rocks = $('.rocks');
-    
+  /**
+    * the fall() targets ALL rocks
+  **/
+  function fall(target) {
+    // var rocks = $('.rocks');
+    var top = rocks.offset().top;
+    var timerId = setInterval(function() {
+      target.css('top', top + 10);
+    }, 1000);
   }
 
 
-  // in loop, update and make rocks fall
-
 
   $('body').on('keydown', move)
+
+  // in loop, update and make rocks fall
+  // var timerId = setInterval(draw, 1000);
 
 });
